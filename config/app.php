346bffcca08ea5c84080cb108af2fe9226bd7ce9
blaine <blain.hw@gmail.com>
2018-10-17 11:35:17 +0800

    User can upload avatar

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Mews\Captcha\CaptchaServiceProvider::class,
-
+        \Intervention\Image\ImageServiceProviderLaravel5::class
     ],
 
     /*
@@ -228,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Captcha' => 'Mews\Captcha\Facades\Captcha',
+        'Image' => \Intervention\Image\ImageServiceProviderLaravel5::class,
 
     ],
 
