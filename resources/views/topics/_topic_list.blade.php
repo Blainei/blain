b30a0e2c378538e9b6803f7339c2f3c794e20492
blaine <blain.hw@gmail.com>
2018-10-17 16:54:57 +0800

    Auto translate slug

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -15,7 +15,7 @@
                         {{ $topic->category->name }}
                     </a>
                     <div class="media-heading">
-                        <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
+                        <a href="{{ $topic->link() }}" title="{{ $topic->title }}" title="{{ $topic->title }}">
                             {{ $topic->title }}
                         </a>
                         <a class="pull-right" href="{{ route('topics.show', [$topic->id]) }}">
